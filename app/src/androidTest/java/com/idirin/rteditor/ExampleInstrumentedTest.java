36a9a7b49cd3290c214b258185a79bceba6eac2e
İlyas Dirin <idirin@hurriyetemlak.com>
2017-05-01 22:15:16 +0300

    App Id revised.

--- app/src/androidTest/java/com/idirin/rteditor/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/idirin/rteditor/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.idirin.rteditor2", appContext.getPackageName());
+        assertEquals("com.idirin.rteditor", appContext.getPackageName());
     }
 }
